1627184a5689e901b08fcfeb5fbd5718f6ef12a6
Hrachya Yeghishyan <hrach97@gmail.com>
2021-07-20 19:49:33 +0400

    add Main class

---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        //I'm going to print out on the console
+ //this is a comment       System.out.println("hi guys!");  //yet another comment
+    }
+}
